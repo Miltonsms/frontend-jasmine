f6e3120f2c8b930f75f47b21ab92f8ea8fcf0e85
Miltonsms <milsmsramirezz@gmail.com>
2015-01-20 12:28:37 -0600

    practica

--- spec/spec.js
+++ spec/spec.js
@@ -3,5 +3,24 @@ describe("First jasmine project", function() {
 
     expect(helloWorld()).toBe("hello world");
   });
+   it("it shoul write jasmine", function() {
+
+    expect(title).toBe("practicar de jasmine");
+    expect(title).not.toBe(" ");
+
+  });
+    it("it shoul include in the  jasmine", function() {
+
+    expect(title).toMatch(/jasmine/i);
+  });
+    it("verificacion de suma", function() {
+    expect(suma(2,2)).toBe(4);
+    expect(suma(3,6)).toBe(9);
+    expect(suma(-50,1000)).toBe(950);
+    expect(suma("50","400")).toBe(450);
+     expect(suma("this is a string",50)).toBe(null);
+
+      });
 
 });
+
--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
 function helloWorld() {
   return "hello world";
 }
+
+var title = "practicar de jasmine"
+var valor1  =  ""
+var valor2  =  ""
+function titulo() {
+  document.write('<h1>'+title+'</h1>')
+  
+};
+titulo();
+function suma(valor1,valor2) {
+	var suma = valor1+valor2
+};
+
+
+
